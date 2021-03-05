448ba3c368014a3163274df9989a508cdaf54fcf
BeeSwapFinance <beeswapfinance@gmail.com>
2021-03-05 01:26:59 +0100

    Rebranding - updating images, updating contracts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -42,7 +42,7 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'PancakeSwap',
-        href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+        href: 'https://pancakeswap.info/token/0x3aEb6DeE52eD6772f26C68bAc63f121121d7092F',
       },
       {
         label: 'CoinGecko',
@@ -72,19 +72,9 @@ const config: MenuEntry[] = [
       },
       {
         label: "Blog",
-        href: "https://medium.com/@beeswapfinance/",
+        href: "https://beeswapfinance.medium.com/",
       },
     ],
-  },
-  {
-    label: 'Audit by Hacken',
-    icon: 'AuditIcon',
-    href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
-  },
-  {
-    label: 'Audit by CertiK',
-    icon: 'AuditIcon',
-    href: 'https://certik.org/projects/goose-finance',
   },
 ]
 
--- src/constants/beePriceContracts.ts
+++ src/constants/beePriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
+  cakeAddress: '0x3aEb6DeE52eD6772f26C68bAc63f121121d7092F',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
+  lpAddress: '0xa7790950dd0a314380fa84e5dc584023568c8095'
 }
 
 export default priceContracts
