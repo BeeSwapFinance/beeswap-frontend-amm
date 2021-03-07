bb21300f8d158b6b67e79d88a22290d9f971b2b1
BeeSwapFinance <beeswapfinance@gmail.com>
2021-03-07 10:50:36 +0100

    Pre-release

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -6,58 +6,58 @@ const config: MenuEntry[] = [
     icon: 'HomeIcon',
     href: 'https://www.beeswaps.com/'
   },
-  {
-    label: 'Trade',
-    icon: 'TradeIcon',
-    initialOpenState: true,
-    items: [
-      {
-        label: 'Exchange',
-        href: '/swap'
-      },
-      {
-        label: 'Liquidity',
-        href: '/pool'
-      }
-    ]
-  },
-  {
-    label: 'Farms',
-    icon: 'FarmIcon',
-    href: 'https://www.beeswaps.com/farms'
-  },
-  {
-    label: 'Hives',
-    icon: 'PoolIcon',
-    href: 'https://www.beeswaps.com/nests'
-  },
+  // {
+  //   label: 'Trade',
+  //   icon: 'TradeIcon',
+  //   initialOpenState: true,
+  //   items: [
+  //     {
+  //       label: 'Exchange',
+  //       href: '/swap'
+  //     },
+  //     {
+  //       label: 'Liquidity',
+  //       href: '/pool'
+  //     }
+  //   ]
+  // },
+  // {
+  //   label: 'Farms',
+  //   icon: 'FarmIcon',
+  //   href: 'https://www.beeswaps.com/farms'
+  // },
+  // {
+  //   label: 'Hives',
+  //   icon: 'PoolIcon',
+  //   href: 'https://www.beeswaps.com/nests'
+  // },
   // {
   //   label: 'Lottery',
   //   icon: 'TicketIcon',
   //   href: 'https://www.goosedefi.com/lottery'
   // },
-  {
-    label: 'Info',
-    icon: 'InfoIcon',
-    items: [
-      {
-        label: 'PancakeSwap',
-        href: 'https://pancakeswap.info/token/0x1baB59784786b7D5B89934BFFB1e2942E265bD6D',
-      },
-      {
-        label: 'CoinGecko',
-        href: 'https://www.coingecko.com/en/coins/goose-finance',
-      },
-      {
-        label: 'CoinMarketCap',
-        href: 'https://coinmarketcap.com/currencies/goose-finance/',
-      },
-      {
-        label: 'AstroTools',
-        href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
-      },
-    ]
-  },
+  // {
+  //   label: 'Info',
+  //   icon: 'InfoIcon',
+  //   items: [
+  //     {
+  //       label: 'PancakeSwap',
+  //       href: 'https://pancakeswap.info/token/0x1baB59784786b7D5B89934BFFB1e2942E265bD6D',
+  //     },
+  //     {
+  //       label: 'CoinGecko',
+  //       href: 'https://www.coingecko.com/en/coins/goose-finance',
+  //     },
+  //     {
+  //       label: 'CoinMarketCap',
+  //       href: 'https://coinmarketcap.com/currencies/goose-finance/',
+  //     },
+  //     {
+  //       label: 'AstroTools',
+  //       href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
+  //     },
+  //   ]
+  // },
   {
     label: 'More',
     icon: 'MoreIcon',
