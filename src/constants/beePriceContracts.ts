952a39bc84a6760a0255aebe0fb22b0d7bf9f5a4
BeeSwapFinance <beeswapfinance@gmail.com>
2021-03-10 13:31:17 +0100

    Fix exchange

--- src/constants/beePriceContracts.ts
+++ src/constants/beePriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0x1baB59784786b7D5B89934BFFB1e2942E265bD6D',
+  cakeAddress: '0xf60011fF21F44E1faf249E084CCE26FeA88a08F7',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0xa7790950dd0a314380fa84e5dc584023568c8095'
+  lpAddress: '0x10b8b3418E6BfAbCA16CB83f1Ba95581E5723B3F'
 }
 
 export default priceContracts
